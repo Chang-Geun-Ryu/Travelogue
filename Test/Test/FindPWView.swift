c088c57063a602ba4a14fea00f2629816fc33a02
ChangGeun Ryu <changgeunryu@ChangGeunui-MacBookAir.local>
2021-01-12 11:17:23 +0900

    test update 2

--- Test/Test/FindPWView.swift
+++ Test/Test/FindPWView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct FindPWView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.init("backgroundColor").edgesIgnoringSafeArea(.all)
+            Text("Hello, World!")
+        }.navigationBarTitle("SwiftUI", displayMode: .inline)
     }
 }
 

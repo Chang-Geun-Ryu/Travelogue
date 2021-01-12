2a8cae51f777d3a3e51a2f6553bb28f64ea8380a
ChangGeun Ryu <changgeunryu@ChangGeunui-MacBookAir.local>
2021-01-12 11:16:21 +0900

    Test update

---
+++ Test/Test/FindPWView.swift
@@ -0,0 +1,20 @@
+//
+//  FindPWView.swift
+//  Test
+//
+//  Created by mcnc on 2021/01/12.
+//
+
+import SwiftUI
+
+struct FindPWView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindPWView_Previews: PreviewProvider {
+    static var previews: some View {
+        FindPWView()
+    }
+}
